2f075ec488c076142981f0fff95bf14e6e129fab
Matt Levy <milevy1@gmail.com>
2019-01-22 17:35:09 -0700

    Added test_turn_has_a_card

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,4 +12,11 @@ class CardTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
+  def test_turn_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
+
 end
