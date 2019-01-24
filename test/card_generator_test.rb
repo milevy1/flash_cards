c0576017e1c09d646f31b56e6eefef9a003417eb
Matt Levy <milevy1@gmail.com>
2019-01-23 20:36:55 -0700

    Refactored test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -9,9 +9,9 @@ require './lib/card_generator'
 class CardGeneratorTest < Minitest::Test
 
   def test_card_generator_can_open_file
-    cards = CardGenerator.new('./lib/cards.txt')
+    cards = CardGenerator.new('./data/cards.txt')
 
-    assert_equal cards.filename, './lib/cards.txt'
+    assert_equal cards.filename, './data/cards.txt'
   end
 
 end
