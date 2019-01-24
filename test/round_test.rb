ebdad01739c00f852049875462f0090a1c63c44c
Matt Levy <milevy1@gmail.com>
2019-01-23 17:12:52 -0700

    Added test that current card changes after taking a turn

--- test/round_test.rb
+++ test/round_test.rb
@@ -80,7 +80,7 @@ class RoundTest < Minitest::Test
     assert_equal [new_turn], round.turns
   end
 
-  def test_round_turns_returns_array_of_turns_after_taking_a_turn
+  def test_round_number_correct_returns_1
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -89,6 +89,17 @@ class RoundTest < Minitest::Test
     new_turn = round.take_turn("Juneau")
 
     assert_equal 1, round.number_correct
+  end
+
+  def test_round_current_card_changes_after_taking_a_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal card_2, round.current_card
   end
 
 
