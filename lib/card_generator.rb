c629e2475ffc48d911d96e64f9e9cd811d84c400
Matt Levy <milevy1@gmail.com>
2019-01-23 20:38:12 -0700

    Refactored code

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,7 +7,9 @@ class CardGenerator
     # Read in data, split by new lines, then map to array split by the commas
     @filedata = File.open(filename, 'r').read.split(/\n/).map{ |card_string| card_string.split(',') }
     # Then shuffle each elemeet of the array into the cards
-    @filedata.each{ |card| @cards << Card.new(card[0], card[1], card[2]) }
+    @filedata.each{ |card|
+      @cards << Card.new(card[0], card[1], card[2])
+    }
   end
 
 end
