2d499dc76da6b488e35e61ea02c46059716d510f
Matt Levy <milevy1@gmail.com>
2019-01-25 16:37:25 -0700

    Refactored turn_test with a setup method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,39 +5,29 @@ require './lib/turn'
 
 class CardTest < Minitest::Test
 
-  def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
 
-    assert_instance_of Turn, turn
+  def test_it_exists
+    assert_instance_of Turn, @turn
   end
 
   def test_turn_has_a_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal card, turn.card
+    assert_equal @card, @turn.card
   end
 
   def test_turn_has_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
   def test_turn_correct_method_returns_true
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert turn.correct?
+    assert @turn.correct?
   end
 
   def test_turn_feedback_returns_correct
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Correct!", turn.feedback
+    assert_equal "Correct!", @turn.feedback
   end
 
   def test_turn_correct_method_returns_false
